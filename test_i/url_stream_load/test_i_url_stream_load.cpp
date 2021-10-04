3891a0bbe8216102485abdcc9b61c4b1ed45a81c
Erik Sohns <erik.sohns@posteo.de>
2021-10-04 18:16:01 +0200

    minor changes

--- test_i/url_stream_load/test_i_url_stream_load.cpp
+++ test_i/url_stream_load/test_i_url_stream_load.cpp
@@ -698,8 +698,6 @@ do_work (bool debugParser_in,
     COMMON_TIMERMANAGER_SINGLETON::instance ();
   ACE_ASSERT (timer_manager_p);
   struct Common_TimerConfiguration timer_configuration;
-//  int group_id = -1;
-  ACE_thread_t thread_id = 0;
 #if defined (GUI_SUPPORT)
 #if defined (GTK_USE)
   Common_UI_GTK_Manager_t* gtk_manager_p = NULL;
